e4760282d1146736eced992d1583ec40e8c6d09c
pgh157 <kh813love@naver.com>
2025-02-06 17:22:20 +0900

    4일차 커밋

---
+++ day04/py04_module.py
@@ -0,0 +1,27 @@
+# 수학모듈 : 수학함수들이 모여있는 파이썬 모듈
+# alt + 화살표 : 구문 이동
+import math
+import random
+
+print(math.pi)
+
+# 2**10 -> int
+print(math.pow(2,10)) #float
+print(math.sqrt(4))
+print(math.log2(2))
+
+numbers = [i for i in range(1,45+1)] # 1 ~ 45 리스트로
+result = []
+for i in range(6): # 여섯번 반복(0~5)
+    result.append(random.choice(numbers))
+
+print(result)
+# 초간단 로또
+
+# weight : 가중치
+numbers = weight = list(range(1,45+1))
+random.shuffle(weight)
+
+result = random.choices(numbers, weights = weight, k = 6)
+print(result)
+# 한꺼번에 여것개를 추출하는 랜덤
